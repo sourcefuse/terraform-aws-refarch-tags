2fdfa642e47c9f45aa75ecb28a943f8f49be947d
Travis Saucier <travis.saucier@sourcefuse.com>
2022-08-01 20:10:58 -0400

    fix pre-commit. run pre-commit.

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   tags = merge(var.extra_tags, tomap({
-    Environment  = var.environment
-    Project      = var.project
+    Environment = var.environment
+    Project     = var.project
   }))
 }
